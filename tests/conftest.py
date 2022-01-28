f5e339f0c316030986e429c43f4216bf956aab3a
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-01-28 11:48:04 +0100

    Apply precommit

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,9 @@ import os
 
 # Read the test data from the schemas subdirectory
 _test_data = []
-for filename in glob.glob(os.path.join(os.path.split(__file__)[0], "schemas", "*.json")):
+for filename in glob.glob(
+    os.path.join(os.path.split(__file__)[0], "schemas", "*.json")
+):
     with open(filename, "r") as f:
         _test_data.append(json.load(f))
 
