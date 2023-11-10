4df45d515a539239a693262c271fbe93592f6b94
fkuehnlenz <florian.kuehnlenz@e2m.energy>
2023-11-10 18:19:43 +0100

    suggestions from PR.

--- tests/test_form.py
+++ tests/test_form.py
@@ -24,10 +24,9 @@ def test_form(testcase, preconstruct):
         with pytest.raises((FormError, jsonschema.ValidationError)):
             form.data = doc
 
-@pytest.mark.parametrize("preconstruct", (0, 1))
-def test_form_with_descriptions(testcase, preconstruct):
+def test_form_with_descriptions(testcase):
     # simple smoke test to see if generation with descriptions runs withour errors
-    Form(testcase["schema"], preconstruct_array_items=preconstruct, show_descriptions=True)
+    Form(testcase["schema"], show_descriptions=True)
 
 def test_add_observer(testcase):
     # Try adding an observer
